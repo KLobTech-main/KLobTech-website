e03f46c11926b661735102acc5375a6d51ef9046
Ankitpareek-dev <ankitpareek.dev@gmail.com>
2025-01-15 16:23:22 +0530

    fixing vercel error

--- src/Home.jsx
+++ src/Home.jsx
@@ -24,7 +24,6 @@ import Logo from "./Components/logo";
 import Mobilefooter from "./Mobilefooter";
 import ServicesShowcase from "./ServicesShowcase";
 import Navvv from "./NewNav";
-import ServiceCards from "./Components/ServiceCards/CardComponent";
 
 function Home() {
   const isMobile = useMediaQuery({ query: "(max-width: 768px)" });
